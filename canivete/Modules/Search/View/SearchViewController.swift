2d3f2325edb500fe13a27f3e1f542e5dbd79e768
Fernando <lf.fernandosantos@gmail.com>
2020-09-02 22:12:25 -0300

    Hidden keyboard.

--- canivete/Modules/Search/View/SearchViewController.swift
+++ canivete/Modules/Search/View/SearchViewController.swift
@@ -18,46 +18,64 @@ final class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.searchController = UISearchController(searchResultsController: nil)
-        self.searchController?.searchResultsUpdater = self
-        self.searchController?.searchBar.placeholder = "Buscar carros"
-        navigationItem.searchController = self.searchController
+        filteredModels = models
+        searchController = UISearchController(searchResultsController: nil)
+        searchController?.searchResultsUpdater = self
+        searchController?.obscuresBackgroundDuringPresentation = false
+        searchController?.searchBar.delegate = self
+        searchController?.searchBar.placeholder = "Buscar carros"
+        navigationItem.searchController = searchController
         
         definesPresentationContext = true
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
-        self.tableView.reloadData()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
+    }
+    
+    func hideKeyboard() {
+        searchController?.searchBar.resignFirstResponder()
+        searchController?.searchBar.endEditing(true)
     }
     
     func filterModelForSearchText(_ searchText: String) {
-        self.filteredModels = self.models.filter { $0.lowercased().contains(searchText.lowercased()) }
-        self.tableView.reloadData()
+        filteredModels = models.filter { $0.lowercased().contains(searchText.lowercased()) }
+        if filteredModels.count == 0 {
+            filteredModels = models
+        }
+        tableView.reloadData()
     }
 
 }
 
 extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.filteredModels.count
+        return filteredModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = self.filteredModels[indexPath.row]
+        cell.textLabel?.text = filteredModels[indexPath.row]
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        hideKeyboard()
+    }
 }
 
 extension SearchViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         guard let searchText = searchController.searchBar.text else { return }
-        self.filterModelForSearchText(searchText)
-        
-
-        
+        filterModelForSearchText(searchText)
     }
-    
-    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        hideKeyboard()
+    }
 }
